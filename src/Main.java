3e6a2ba12c904be71972c558785d1fd617b0e788
Mihael Petrovic <mihael.petrovic1@hotmail.com>
2025-10-22 18:37:36 +0200

    Inicijalni commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
