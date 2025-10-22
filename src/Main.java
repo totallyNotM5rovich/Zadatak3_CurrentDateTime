0e5d9562a9f93b13c41872b8a00ee814805d1749
totallyNotM5rovich <147877372+totallyNotM5rovich@users.noreply.github.com>
2025-10-22 18:41:59 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         LocalDateTime dt = LocalDateTime.now();
         System.out.println("Trenutni datum i vrijeme su: " + dt);
+        //Komentar iz GitHub repozitorija
     }
 }
